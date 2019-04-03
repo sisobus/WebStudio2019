9538b41d3895dddb2455d54c3ab5f482c89dd9b1
wakii <cwh6997@gmail.com>
2019-04-03 19:15:02 +0900

    add Week 4 Completed

--- 4_flask_restful/practice/wakii/api.py
+++ 4_flask_restful/practice/wakii/api.py
@@ -6,6 +6,11 @@ import os
 app = Flask(__name__)
 api = Api(app)
 
+def get_id(l):
+	_id = 0
+	for d in l:
+		_id = max(d['id'],_id)
+	return _id +1	
 class UserList(Resource):
 	filename = 'users.json'
 
@@ -32,12 +37,13 @@ class UserList(Resource):
 		email = r_json['email']
 		password = r_json['password']
 		r = self.get_users()
-		_id = 0
+#		_id = 0
 		for d in r:
 			if email == d['email']:
 				return '{} already exists.' 
-			_id = max(_id,d['id'])
-		_id +=1
+#			_id = max(_id,d['id'])
+#		_id +=1
+		_id = get_id(r)		
 		r_json['id'] = _id	
 		r.append(r_json)
 		with open(self.filename, 'w') as fp:
@@ -46,34 +52,36 @@ class UserList(Resource):
 
 	def put(self):
 		r_json = request.get_json()
-		email = r_json['email']
+	#	email = r_json['email']
+		_id = r_json['id']
 		password = r_json['password']
 		users = self.get_users()
 		found = False
 		for idx, _ in enumerate(users):
-			if users[idx]['email'] == email:
+			if users[idx]['id'] == _id:
 				found = True
 				users[idx]['password'] == password
 		if not found :
-			return '{} not exists'.format(email)
+			return '{} not exists'.format(_id)
 		with open(self.filename, 'w') as fp:
 			fp.write(json.dumps(users))
 		return 'update password successfully'
 		
 	def delete(self):
 		r_json = request.get_json()
-		email = r_json('email')
+	#	email = r_json('email')
+		_id = r_json['id']
 		users = self.get_user()
 		found = False
 		for idx, _ in enumerate(users):
-			if users[idx]['email'] == email:
+			if users[idx]['id'] == _id:
 				found = True
 				del users[idx]
 		if not found:
-			return '{} not exists'.format(email)
+			return '{} not exists'.format(_id)
 		with open(self.filename, 'w') as fp:
 			fp.write(json.dumps(users))
-		return '{} deleted successfully'.format(email)
+		return '{} deleted successfully'.format(_id)
 
 class Article(Resource):
 	filename = 'articles.json'
@@ -88,22 +96,20 @@ class Article(Resource):
 	def get(self):
 		if not os.path.exists(self.filename):
 			return 'File \'Articles.json\' not exists'
-		r = self.get_articles()
-		s = ''
-		for d in r:
-			s+= '\n [User_id :{}, Title : {}, Content : {}]'.format(d['user_id'],d['title'],d['content'])
-		return s 
-
+	#	r = self.get_articles()
+	#	s = ''
+	#	for d in r:
+	#		s+= '\n [User_id :{}, Title : {}, Content : {}]'.format(d['user_id'],d['title'],d['content'])
+	#	return s 
+		return json.dumps(self.get_articles())	
+	
 	def post(self):
 		r_json = request.get_json()
 		user_id = r_json['user_id']
 		title = r_json['title']
 		content = r_json['content']
-		articles = []
-		_id = 0
-		for d in articles:
-			_id = max(_id,d['id'])
-		_id +=1
+		articles = self.get_articles() # 오답노트
+		_id = get_id(articles)		
 		r_json['id'] = _id
 		articles.append(r_json)	
 		with open(self.filename, 'w') as fp :
@@ -112,18 +118,17 @@ class Article(Resource):
 			
 	def put(self):
 		r_json = request.get_json()
-	#	user_id = r_json['user_id']
+		_id = r_json['id']
 		title = r_json['title']
 		content = r_json['content']
-		article_id = r_json['id']
 		articles = self.get_articles()
 		for d in articles:
-			if d['id'] == article_id:
+			if d['id'] == _id:
 				d['title'] = title
 				d['content'] = content
 			break
 		else :
-			return 'Article {} has not found.'.format(article_id)
+			return 'Article {} has not found.'.format(_id)
 		with open(self.filename, 'w') as fp:
 			fp.write(json.dumps(articles))
 		return 'Update Completed'
@@ -135,18 +140,18 @@ class Article(Resource):
 		articles = self.get_articles()
 		for d in articles:
 			if d['id'] == _id:
-				del d
+				articles.remove(d)
 				break
 		else :
 			return 'Article {} has not found.'.format(_id)
 		with open(self.filename, 'w') as fp:
 			fp.write(json.dumps(articles))
-		return '{} has been deleted.'.format(article_id)
+		return '{} has been deleted.'.format(_id)
 		
 class CommentList(Resource):
 	filename = 'comments.json'
 
-	def get_comments():
+	def get_comments(self):
 		comments = []
 		if os.path.exists(self.filename):
 			with open(self.filename, 'r') as fp:
@@ -156,11 +161,12 @@ class CommentList(Resource):
 	def get(self):
 		if not os.path.exists(self.filename):
 			return 'File \'comments.json\' not exists.'
-		r = self.get_comments()
-		s = ''
-		for d in r:
-			s += '\n [User ID : {}, Article ID : {}, Content : {}]'
-		return s
+#		r = self.get_comments()
+#		s = ''
+#		for d in r:
+#			s += '\n [User ID : {}, Article ID : {}, Content : {}]'
+#		return s
+		return json.dumps(self.get_comments())
 	
 	def post(self):
 		r_json = request.get_json()
@@ -168,10 +174,7 @@ class CommentList(Resource):
 		article_id = r_json['article_id']
 		content = r_json['content']
 		comments = self.get_comments()
-		_id = 0
-		for d in comments :
-			_id = max(d['id'], _id)
-		_id +=1
+		_id = get_id(comments)
 		r_json['id'] = _id
 		comments.append(r_json)
 		with open(self.filename, 'w') as fp:
@@ -197,22 +200,56 @@ class CommentList(Resource):
 		r_json = request.get_json()
 		_id = r_json['id']
 		comments = self.get_comments()
-		for d in comments:
-			if d['id'] == _id:
-				del d
-				break
-		else :
-			return 'Comment {} has not found.'.format(_id)
+		new_comments = []
+		for comment in comments:
+			if comment['id'] == _id:
+				continue
+			new_comments.append(comment)
 		with open(self.filename, 'w') as fp :
-			fp.write(json.dumps(comments))
+			fp.write(json.dumps(new_comments))
 		return '{} has been deleted.'.format(_id)			
 			
+class LikeList(Resource):
+	filename = 'likes.json'
+	
+	def get_likes(self):
+		likes = []
+		if os.path.exists(self.filename):
+			with open(self.filename, 'r') as fp:
+				likes = json.loads(fp.read())
+		return likes
+	
+	def get(self):
+		return json.dumps(self.get_likes())
 
+	def post(self):
+		r_json = request.get_json()
+		user_id = r_json['user_id']
+		articles_id = r_json['article_id']
+		likes = self.get_likes()
+		new_likes = []
+		found = True
+		for like in likes:
+			if like['user_id'] == user_id and articles_id == like['article_id'] :
+				found = True
+				continue
+			new_likes.append(like)
+		if not found :
+			_id = get_id(likes)
+			r_json['id'] = _id
+			new_likes.append(r_json)
+		if found :
+			return "Unlike Successfully"
+		with open(self.filename, 'w') as fp:
+			fp.write(json.dumps(new_likes))
+		return "like Successfully"
+		
 
 
 api.add_resource(UserList, '/api/users/')
 api.add_resource(Article, '/api/articles')
 api.add_resource(CommentList, '/api/comments')
+api.add_resource(LikeList, '/api/likes')
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port = 5000, debug=True)
