98e3ac0013fbfa2a1a33292154d0289f3ccb799b
강영진 <Kang@macui-MacBook-Pro-2.local>
2019-04-16 12:32:55 +0900

    8-practice-KangYeongJin

--- 8_javascript/practice/Sangkeun/server.py
+++ 8_javascript/practice/Sangkeun/server.py
@@ -200,4 +200,4 @@ api.add_resource(LikeList, '/api/likes')
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5002, debug=True)
