c79f4360942b7fd0f097f523681fa49987efb7bb
Eunsoo Cho <eunsoocho@Eunsooui-MacBook-Pro.local>
2019-05-08 21:10:44 +0900

    add new files

---
+++ 10_antd/mytestapp/src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './App.css';
+import { Button, DatePicker } from 'antd'
+import Sider from './Sider'
+
+const { RangePicker } = DatePicker;
+
+function onChange(date, dateString){
+  consold.log(date, dateString);
+}
+
+function App(){
+  return (
+   <div className='App'>
+     <div className='sider'>
+       <Sider  />
+      </div>
+   <div className='main-container'>
+    <div style=[{  padding: 20  }]>
+    <Button type="primary">Primary</Button>
+    <Button>Default</Button>
+    <Button type="dashed">Dashed</Button>
+    <Button type="danger">Danger</Button>
+    <Button type="link">Link</Button>
+  </div>
+  <div style=[{padding: 20}]>
+  <RangePicker onChange={onChange}/>
+  </div>
+  </div>
+  </div>
+)
+}
+
+export default App;
