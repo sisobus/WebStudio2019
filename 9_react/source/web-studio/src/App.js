f3b3eab6145520dcf4eabfb48bff22516a8c371a
Dpseh <tnql7378@gmail.com>
2019-05-22 20:45:14 +0900

    Add prev directories

---
+++ 9_react/source/web-studio/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import MainPage from './_components/Main'
+
+function App() {
+  return (
+    <div className="App">
+      <MainPage />
+    </div>
+  );
+}
+
+export default App;
