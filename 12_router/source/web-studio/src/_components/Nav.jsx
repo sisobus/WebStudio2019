98f0e0e29ad15ba422a50f0ed4d4d634728f82b1
sisobus <sisobus1@gmail.com>
2019-05-27 18:35:34 +0900

    :memo: Add slides lecture 12

---
+++ 12_router/source/web-studio/src/_components/Nav.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Nav.css'
+
+class Nav extends React.Component {
+  render() {
+    return (
+      <div className="navbar-wrapper">
+        <div className="navbar-container">
+          <p id="logo">LOGO</p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Nav
