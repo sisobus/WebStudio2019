51eade5b167cd8bc8a8b3b1e5dba7fe60e00ac2c
yeongmo-j <yeongmoj94@gmail.com>
2019-05-28 13:05:05 +0900

    make structure of the page

--- projects/20131811/web/src/App.js
+++ projects/20131811/web/src/App.js
@@ -1,32 +1,41 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route} from "react-router-dom";
+
+import Head from './components/Head';
+import Page from './components/Page';
+import ReviewDate from './components/ReviewDate';
+import ReviewStar from './components/ReviewStar';
+import Sidebar from './components/Sidebar';
+import Upload from './components/Upload';
+
+import { Layout } from 'antd';
+
+const { Header, Footer, Sider, Content } = Layout;
 
 class App extends Component {
-
-  //제일 처음 실행
-  constructor(props) {
-    super(props);
-    this.state = {
-        datas: []
-    }
-    
-    fetch('http://localhost:8080/article/list')
-    .then(res => res.json())
-    .then(data => this.setState({
-        datas: data
-    }))
-    .then(console.log(this.state));
-  }
-  //그다음 실행
-  componentWillMount() {
-  }
-
-  //Article에 데이터 전달
   render () {
     return (
       <div className="App">
-        <Article data={this.state}/>
+        <Layout>
+          <Header>
+            <Head/>
+          </Header>
+          <Layout>
+            <Sider><Sidebar/></Sider>
+            <Content>
+              <Router>
+                <Route path="/" exact component={ReviewDate}/>
+                <Route path="/reviewdate" exact component={ReviewDate}/>
+                <Route path="/reviewstar" exact component={ReviewStar}/>
+                <Route path="/page" exact component={Page}/>
+                <Route path="/upload" exact compontnt={Upload}/>
+              </Router>
+            </Content>
+          </Layout>
+          <Footer>Footer</Footer>
+        </Layout>
       </div>
     );
   }
---
+++ projects/20131811/web/src/components/ReviewDate.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+
+
+class ReviewDate extends Component {
+
+    //제일 처음 실행
+    constructor(props) {
+      super(props);
+      this.state = {
+          datas: []
+      }
+      /*
+      fetch('http://localhost:8080/article/list')
+      .then(res => res.json())
+      .then(data => this.setState({
+          datas: data
+      }))
+      .then(console.log(this.state));
+      */
+    }
+    //그다음 실행
+    componentWillMount() {
+    }
+  
+    //Article에 데이터 전달
+    render () {
+      return (
+        <div>
+            
+        </div>
+      );
+    }
+  }
+  
+  export default ReviewDate;
+  
