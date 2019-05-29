71f69068450dcf5587729b16e268e58e4acee511
junoflow <joonho@DESKTOP-3FO66OI.localdomain>
2019-05-29 21:07:47 +0900

    make sth

---
+++ projects/20151172/web/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+	return (
+		<div className="container">
+	  		<h1>Hustle Life</h1>
+			<input class="container_toggle" type="checkbox" id="switch" name="mode">
+			<label for="switch">Toggle</label>
+		</div>
+	);
+}
+
+export default App;
