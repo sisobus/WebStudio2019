f79fe1d43c75b7d69db6abd353faea8e0a164dd7
baehatrice <baehatrice@ip-172-31-21-135.ap-northeast-2.compute.internal>
2019-06-03 11:30:23 +0000

    0603

--- projects/20171229/api/boilerplate.py
+++ projects/20171229/api/boilerplate.py
@@ -26,42 +26,43 @@ class WorkList(Resource):
         return {'raw': base64.urlsafe_b64encode(message.as_string())}
 
     def create_message_with_attachment(
-    sender, to, subject, message_text, file):
-        message = MIMEMultipart()
-  message['to'] = to
-  message['from'] = sender
-  message['subject'] = subject
+     sender, to, subject, message_text, file):
+     message = MIMEMultipart()
+     message['to'] = to
+     message['from'] = sender
+     message['subject'] = subject
 
-  msg = MIMEText(message_text)
-  message.attach(msg)
+     msg = MIMEText(message_text)
+     message.attach(msg)
 
-  content_type, encoding = mimetypes.guess_type(file)
+     content_type, encoding = mimetypes.guess_type(file)
 
-  if content_type is None or encoding is not None:
-    content_type = 'application/octet-stream'
-  main_type, sub_type = content_type.split('/', 1)
-  if main_type == 'text':
-    fp = open(file, 'rb')
-    msg = MIMEText(fp.read(), _subtype=sub_type)
-    fp.close()
-  elif main_type == 'image':
-    fp = open(file, 'rb')
-    msg = MIMEImage(fp.read(), _subtype=sub_type)
-    fp.close()
-  elif main_type == 'audio':
-    fp = open(file, 'rb')
-    msg = MIMEAudio(fp.read(), _subtype=sub_type)
-    fp.close()
-  else:
-    fp = open(file, 'rb')
-    msg = MIMEBase(main_type, sub_type)
-    msg.set_payload(fp.read())
-    fp.close()
-  filename = os.path.basename(file)
-  msg.add_header('Content-Disposition', 'attachment', filename=filename)
-  message.attach(msg)
+    if content_type is None or encoding is not None:
+      content_type = 'application/octet-stream'
+    main_type, sub_type = content_type.split('/', 1)
+ 
+    if main_type == 'text':
+      fp = open(file, 'rb')
+      msg = MIMEText(fp.read(), _subtype=sub_type)
+      fp.close()
+    elif main_type == 'image':
+       fp = open(file, 'rb')
+       msg = MIMEImage(fp.read(), _subtype=sub_type)
+       fp.close()
+    elif main_type == 'audio':
+      fp = open(file, 'rb')
+      msg = MIMEAudio(fp.read(), _subtype=sub_type)
+      fp.close()
+    else:
+      fp = open(file, 'rb')
+      msg = MIMEBase(main_type, sub_type)
+      msg.set_payload(fp.read())
+      fp.close()
+    filename = os.path.basename(file)
+    msg.add_header('Content-Disposition', 'attachment', filename=filename)
+    message.attach(msg)
 
-  return {'raw': base64.urlsafe_b64encode(message.as_string())}
+     return {'raw': base64.urlsafe_b64encode(message.as_string())}
 
 
 
@@ -70,12 +71,11 @@ class WorkList(Resource):
         try:
             message = (service.users().messages().send(userId=user_id, body=message)
                .execute())
-    print 'Message Id: %s' % message['id']
-    return message
-    except errors.HttpError, error:
-    print 'An error occurred: %s' % error
+            print 'Message Id: %s' % message['id']
+            return message
+        except errors.HttpError, error:
+            print 'An error occurred: %s' % error
    
-
 
 api.add_resource(WorkList, '/api/users')
 
