93ff538f6514d9716d1823cabb48c94ffeea826a
Eunsoo Cho <eunsoocho@Eunsooui-MacBook-Pro.local>
2019-06-10 14:56:19 +0900

    edit main.py

--- projects/20160336/main.py
+++ projects/20160336/main.py
@@ -1,9 +1,29 @@
+from app import app
 from flask import Flask 
+import os
+from datetime import datetime
 
-app = Flask(__name__)
+@app.route('/counter') 
+def counter():
+	filename = 'visit_counter.txt'
+	count = 0
+	if not os.path.exists(filename):
+		with open(filename, 'w') as fp:
+			fp.write(str(count))
 
-@app.route('/') 
-def hello():
-	return 'Hello, World!'
+	with open(filename, 'r') as fp:
+		count = int(fp.read())
+	count+=1
+	with open(filename, 'w') as fp:
+		fp.write(str(count))
+	return str(count)
+
+@app.route('/current_date')
+def current_date():
+	current = datetime.now()
+	return current.strftime("%Y-%m-%d %H:%M:%S" )
+
 if __name__ == '__main__':
-	 app.run()
+	 app.run(debug=True)
+
+
