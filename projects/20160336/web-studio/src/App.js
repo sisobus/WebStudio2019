6b8cf5a1bac47d27ecd75ca74a97cdf9023868d2
Eunsoo Cho <you@example.com>
2019-06-12 18:24:00 +0900

    modified App.js

--- projects/20160336/web-studio/src/App.js
+++ projects/20160336/web-studio/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import MainPage from './_components/Main'
 
 function App() {
   return (
     <div className="App">
-	<p>Hello</p>
+      <MainPage />
     </div>
   );
 }
