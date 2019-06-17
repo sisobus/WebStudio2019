2cfd5e757d999ad91a223ddb2b553894c6826a84
Eunsoo Cho <you@example.com>
2019-06-17 19:06:11 +0900

    error appears to continue

--- projects/20160336/readingapp/src/App.js
+++ projects/20160336/readingapp/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import MainPage from './_components/Main'
 import BlankPage from './_components/BlankPage'
-import { Router, Route } from "react-router-dom"
+import { BrowserRouter as Router, Route } from "react-router-dom"
 import { history } from './_components/history'
 
 function App() {
--- projects/20160336/readingapp/src/_components/Main.jsx
+++ projects/20160336/readingapp/src/_components/Main.jsx
@@ -2,12 +2,17 @@ import React from 'react'
 import './Main.css'
 import Nav from './Nav'
 import Articles from './Articles'
+import { Button } from 'antd'
+import { Link } from 'react-router-dom'
 
 class MainPage extends React.Component {
   render() {
     return (
       <React.Fragment>
         <Nav />
+        <Link to "/blank">
+	  <Button type="primary">Go to Blank</Button>
+	</Link>
         <div className="main-wrapper">
           <div className="main-container">
             <Articles />
