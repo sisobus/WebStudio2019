760e5e5f33e760e02be05411feeb1508463c5617
Eunsoo Cho <you@example.com>
2019-06-25 13:02:37 +0900

    won't load

--- projects/movie_app/src/App.js
+++ projects/movie_app/src/App.js
@@ -17,7 +17,12 @@ class App extends Component{
 	_renderMovies = () => {
 		const movies = this.state.movies.map((movie) => {
 			console.log(movie)
-			return <Movie title={movie.title} poster={movie.large_cover_image} key={movie.id}/>
+			return <Movie 
+			title={movie.title_english} 
+			poster={movie.large_cover_image} 
+			key={movie.id}
+			genres={movie.genres}
+			/>
 		})
 		return movies
 	}
