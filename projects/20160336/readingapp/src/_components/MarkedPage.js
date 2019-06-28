217822614f172c87f20f334432c49b1fadebc5a9
Eunsoo Cho <you@example.com>
2019-06-29 08:37:07 +0900

    wish list

--- projects/20160336/readingapp/src/_components/MarkedPage.js
+++ projects/20160336/readingapp/src/_components/MarkedPage.js
@@ -5,7 +5,15 @@ import { history } from './history'
 import Nav from './Nav'
 import SideMenu from './SideMenu'
 import Foot from './Foot.jsx'
+import { List, Typography, Checkbox } from 'antd';
 
+const data = [
+  ' Davinci Code ',
+  ' Juno ',
+  ' My Fair Lady ',
+  ' the Matrix Series ',
+  ' Happy Death Day ',
+];
 const { Header, Content, Footer, Sider } = Layout;
 
 class MarkedPage extends React.Component {
@@ -16,7 +24,20 @@ class MarkedPage extends React.Component {
       <Layout style={{ marginLeft: 200 }}>
         <Nav />
         <Content>
-          <h1>Hello! Time to cross off the list!</h1>
+          <h2>Hello! Time to cross off the list!<br/><br/></h2>
+          <h4>This is your movie Wish List</h4>
+          <List style={{
+          }}
+            bordered
+            dataSource={data}
+            renderItem={item => (
+            <List.Item style={{listStyleType: "none"}}>
+              <Typography.Text mark>          
+                <Checkbox>
+                </Checkbox></Typography.Text> {item}
+            </List.Item>
+            )}
+          />
         </Content>
         <Foot/>
         </Layout>
