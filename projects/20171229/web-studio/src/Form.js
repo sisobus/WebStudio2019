d755aed901eff53e7055620ee1f47667a4fff659
Baehatrice <qogkdud108@naver.com>
2019-06-28 22:07:24 +0900

    final!

--- projects/20171229/web-studio/src/Form.js
+++ projects/20171229/web-studio/src/Form.js
@@ -19,17 +19,16 @@ class send extends React.Component {
     e.preventDefault();
     this.props.form.validateFields((err, values) => {
       if (!err) {
-        console.log('Received values of form: ', values, typeof(values));
-      }
+        console.log('Received values of form: ', values, typeof(values))};
 
       fetch('http://127.0.0.1:5000/api/mentions', {
         method: 'POST',
         mode: 'no-cors',
         headers:{ 'Content-Type': 'application/json; charset=utf-8' },
         body: JSON.stringify(values)
-      }).then(res=>res.json())
-        .then(res => console.log(res));
-        
+      }.then(values =>values.json()));
+
+
     });
   };
 
