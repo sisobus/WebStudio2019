5996751cadd07bfef3737def15a66110be8a0af7
Eunsoo Cho <you@example.com>
2019-06-28 21:29:47 +0900

    editing

--- projects/20160336/readingapp/src/_components/Main.jsx
+++ projects/20160336/readingapp/src/_components/Main.jsx
@@ -17,42 +17,41 @@ class MainPage extends React.Component {
         overflow: 'auto',
         height: '100%',
         position: 'fixed',
-        background:'#000000',
+        background:'#24292e',
         color:'#fff'
       }}
     >
-    <div className="logo" />
     <Menu 
     theme="dark" 
     mode="inline" 
-    defaultSelectedKeys={['4']}
-    style={{ lineHeight: '64px', color:'#fff' }}
+    defaultSelectedKeys={['1']}
+    style={{ 
+      lineHeight: '64px', 
+      color:'#fff', 
+      listStyleType: "none",
+      paddingLeft:0 }}
     >
-    <Menu.Item key="1">
-          <span className="nav-text"><img src="https://www.lonelyplanet.com/news/wp-content/uploads/2017/03/16124365_1117534098374780_5466006995590971392_n-e1488475313343.jpg" width="50%"/></span>
+        <Menu.Item key="1">
+        <span className="nav-text">
+            <img src="https://www.lonelyplanet.com/news/wp-content/uploads/2017/03/16124365_1117534098374780_5466006995590971392_n-e1488475313343.jpg" width="50%"/></span>
         </Menu.Item>
         <Menu.Item key="2">
-          <Icon type="video-camera" />
           <span className="nav-text">Watched Movie</span>
         </Menu.Item>
         <Menu.Item key="3">
-          <Icon type="upload" />
           <span className="nav-text">Favorites</span>
         </Menu.Item>
         <Menu.Item key="4">
-          <Icon type="bar-chart" />
           <span className="nav-text">Bookmark</span>
         </Menu.Item>
-        <Menu.Item key="4">
-          <Icon type="bar-chart" />
+        <Menu.Item key="5">
           <span className="nav-text">Log Out</span>
-        </Menu.Item></Menu>
+        </Menu.Item>
+        </Menu>
     </Sider>
     <Layout style={{ marginLeft: 200 }}>
-        <Header>
         <Nav />
         <Form/>
-        </Header>
         <Content><Articles /> </Content>
       <Footer style={{textAlign: 'center', padding: 24}}>Published by WebStudio2019</Footer></Layout>
       </Layout>
