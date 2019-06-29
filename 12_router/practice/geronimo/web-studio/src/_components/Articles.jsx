c7a9b59098c2ce97bddfd7727963c3b0f48bdbba
Sangkeun Kim <sisobus1@gmail.com>
2019-06-29 09:19:54 +0000

    Fix all problems

--- 12_router/practice/geronimo/web-studio/src/_components/Articles.jsx
+++ 12_router/practice/geronimo/web-studio/src/_components/Articles.jsx
@@ -6,7 +6,31 @@ class Articles extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      articles: []
+      articles: [{
+	title:'aaa',
+	content: 'content!1!',
+	author:'San',
+	key:1,
+	idx:1,
+	},{
+        title:'aaa',
+        content: 'content!2!',
+        author:'San',
+        key:2,
+        idx:2,
+	},{
+        title:'aaa',
+        content: 'content!3!',
+        author:'San',
+        key:3,
+        idx:3,
+	{,{
+        title:'aaa',
+        content: 'content!4!',
+        author:'San',
+        key:4,
+        idx:4,
+	}]
     }
     this.handleLike = this.handleLike.bind(this)
 
